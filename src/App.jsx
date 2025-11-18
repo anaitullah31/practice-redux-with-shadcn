a9d7a83d844fddf69ef3436905465f11205d3fdf
anaitullah31 <anaitullah198@gmail.com>
2025-11-18 17:25:04 +0800

    Scaffolding a REACT project for REDUX practice with SHADCN JS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold'>Basic Todo App</h1>
+    </>
+  )
+}
+
+export default App
